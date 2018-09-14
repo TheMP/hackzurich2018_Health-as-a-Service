7078a68547d6901b41b25845697215d0661c3985
Michal Rachtan <m.rachtan@gmail.com>
2018-09-15 00:59:56 +0200

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { PrescriptionPage } from '../pages/prescription/prescription';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +13,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = PrescriptionPage;
 
   pages: Array<{title: string, component: any}>;
 
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Prescription', component: PrescriptionPage }
     ];
 
   }
