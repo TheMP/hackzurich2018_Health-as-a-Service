f8f42c99b5884b7cd5e36a88b9ea029ae9352117
taalizo1 <zongren.liu@swisscom.com>
2018-09-15 08:08:07 +0200

    Add Drug patient info inline HTML stub

--- src/pages/prescription/prescription.ts
+++ src/pages/prescription/prescription.ts
@@ -19,10 +19,12 @@ export class PrescriptionPage {
   prescription: Prescription;
 
   constructor(public navCtrl: NavController,
+              public restProvider: RestProvider,
               public navParams: NavParams) {
 
     this.prescription = navParams.get('prescription');
     console.log(this.prescription);
+
   }
 
   ionViewDidLoad() {
@@ -30,4 +32,12 @@ export class PrescriptionPage {
     console.log(this.prescription);
   }
 
+  // TODO: Could not make inline HTML display work, please have a look!
+  // see drug API at https://health.axa.ch/hack/docs/#drugs
+  drugClicked(drug) {
+    this.restProvider.getPatientDrugInfo(drug.swissmedicIds).then(drugHtml => {
+      console.log(drugHtml);
+      drug.html = drugHtml;
+    });
+  }
 }
