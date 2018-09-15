c928105610a9de4c088c5343c78405a5612d8c44
Michal Rachtan <m.rachtan@gmail.com>
2018-09-15 03:33:21 +0200

    WIP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { PrescriptionPage } from '../prescription/prescription';
+
 
 @Component({
   selector: 'page-home',
@@ -17,6 +19,7 @@ export class HomePage {
     this.barcodeScanner.scan().then(barcodeData => {
       console.log(barcodeData.text);
       this.scannedCode = barcodeData.text;
+      this.navCtrl.push(PrescriptionPage);
     }, (err) => {
         console.log('Error: ', err);
     });
