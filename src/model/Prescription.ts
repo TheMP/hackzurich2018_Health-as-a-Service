8bd428cbf994eca6292e587ca76d6dd140d609a8
taalizo1 <zongren.liu@swisscom.com>
2018-09-15 07:43:06 +0200

    Add num of total tabletts info mation

--- src/model/Prescription.ts
+++ src/model/Prescription.ts
@@ -3,17 +3,22 @@ export class Prescription {
   name: String;
   drugs: Array<object>;
   grams: Array<number> = [];
+  numTablets: Array<number> = [];
 
   constructor(name: String, drugs: Array<object>) {
     this.name = name;
     this.drugs = drugs;
     this.drugs.forEach(() => {
       this.grams.push(this.randomInt(1, 10) * 10);
+      this.numTablets.push(this.randomInt(1, 3));
     });
-
   }
 
   randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  public getTotalNumTablets() {
+    return this.numTablets.reduce((a, b) => a + b, 0);
   }
 }
