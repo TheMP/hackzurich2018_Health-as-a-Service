05ef6ec03ac1dc48e2a439c2c676e945d685d92a
taalizo1 <zongren.liu@swisscom.com>
2018-09-15 14:23:45 +0200

    Fix find doctors async call

--- src/pages/finddoctor/finddoctor.ts
+++ src/pages/finddoctor/finddoctor.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
 import {RestProvider} from "../../providers/rest/rest-provider";
+
 @Component({
   selector: 'page-finddoctor',
   templateUrl: 'finddoctor.html'
@@ -9,18 +10,14 @@ import {RestProvider} from "../../providers/rest/rest-provider";
 export class FindDoctorPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-  coordinates: {
-    lat: number
-    lon: number
-  };
-  provides: Array<Object>;
+  items: Array<{ title: string, note: string, icon: string }>;
+  careProviders: Array<Object>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider,
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public restProvider: RestProvider,
               private geolocation: Geolocation) {
     // If we navigated to this page, we will have an item available as a nav param
-
-
   }
 
   ionViewDidLoad() {
@@ -28,20 +25,21 @@ export class FindDoctorPage {
 
     this.geolocation.getCurrentPosition()
       .then((resp) => {
-        console.log(resp.coords);
-      resp.coords;
-    }).then((value) => {
-      console.log(value);
-      this.restProvider.searchNearestCareProviders(value.lon, value.lat, 100).then(response => {
-        this.provides = response.result;
-        console.log(this.provides);
+        return resp.coords;
+      }).then((value) => {
+        this.restProvider.searchNearestCareProviders(
+          value.longitude,
+          value.latitude,
+          20).then(response => {
+            this.careProviders = response.result;
+            console.log(this.careProviders);
       });
-    }).catch((err) =>
-    console.log(err.message));
+    }).catch((err) => {
+      console.log(err.message)
+    });
 
 
   }
-
 
 
 }
